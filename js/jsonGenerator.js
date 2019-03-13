41ba383d73064954c130a8c516c130a9280e91b2
vincent0628 <vincent0910628007@gmail.com>
2019-03-13 09:18:32 +0800

    add machine

--- js/jsonGenerator.js
+++ js/jsonGenerator.js
@@ -1,22 +1,27 @@
 $(function () {
-    $('#repair').on('click', function(){
-        let data='repair';
-        let jsonFile=print(data);
+    $('#repair').on('click', function () {
+        let data = 'repair';
+        let jsonFile = print(data);
         $("#go").text(JSON.stringify(jsonFile));
     });
-    $('#change').on('click', function(){
-        let data='change';
-        let jsonFile=print(data);
+    $('#change').on('click', function () {
+        let data = 'change';
+        let jsonFile = print(data);
         $("#go").text(JSON.stringify(jsonFile));
     });
-    $('#complaint').on('click', function(){
-        let data='complaint';
-        let jsonFile=print(data);
+    $('#complaint').on('click', function () {
+        let data = 'complaint';
+        let jsonFile = print(data);
         $("#go").text(JSON.stringify(jsonFile));
     });
-    $('#tank').on('click', function(){
-        let data='tank';
-        let jsonFile=print(data);
+    $('#tank').on('click', function () {
+        let data = 'tank';
+        let jsonFile = print(data);
+        $("#go").text(JSON.stringify(jsonFile));
+    });
+    $('#machine').on('click', function () {
+        let data = 'machine';
+        let jsonFile = print(data);
         $("#go").text(JSON.stringify(jsonFile));
     });
 });
@@ -24,14 +29,11 @@ $(function () {
 
 function print(data) {
     let oldDate = new Date(2015, 0, 1);
-    let todayDate = new Date();
     let jsonFile = {};
     for (let i = 1; i <= 50; i++) {
         let seriesNoTailPad = ("000" + i);
         let seriesNoTail = seriesNoTailPad.substr(length - 4);
-        // let seriesNo = "re-".concat(seriesNoTail);
 
-        // let repairDate = randomDate(oldDate, todayDate);
         let repairDaysRange = 30;
         let repairDate = new Date(oldDate.getTime() + repairDaysRange * getRandom(0, 24 * 60 * 60 * 1000));
         oldDate = repairDate;
@@ -173,10 +175,98 @@ function print(data) {
             let id = "" + i;
             jsonFile[id] = tankDetail;
             ////////////////////////////////////////////////////////////////////
+        } else if (data === 'machine') {
+            /////////////////////////////////tank///////////////////////////////
+            let machineDetail = {
+                "1": {
+                    "serialNo": "AK-DSL250",
+                    "name": "AK-DSL250染色機"
+                },
+                "2": {
+                    "serialNo": "AK-DSL500",
+                    "name": "AK-DSL500染色機"
+                },
+                "3": {
+                    "serialNo": "AK-DSL1000",
+                    "name": "AK-DSL1000染色機"
+                },
+                "4": {
+                    "serialNo": "AK-SL100",
+                    "name": "AK-SL100雙液流染色機"
+                },
+                "5": {
+                    "serialNo": "AK-SL250",
+                    "name": "AK-SL250雙液流染色機"
+                },
+                "6": {
+                    "serialNo": "AK-SL500",
+                    "name": "AK-SL500雙液流染色機"
+                },
+                "7": {
+                    "serialNo": "AK-SL1000",
+                    "name": "AK-SL1000雙液流染色機"
+                },
+                "8": {
+                    "serialNo": "AK-ISL300",
+                    "name": "AK-ISL300雙液流染色機"
+                },
+                "9": {
+                    "serialNo": "AK-ISL600",
+                    "name": "AK-ISL600雙液流染色機"
+                },
+                "10": {
+                    "serialNo": "AK-ISL1200",
+                    "name": "AK-ISL1200雙液流染色機"
+                },
+                "11": {
+                    "serialNo": "AK-REO 1T",
+                    "name": "AK-REO 1T常溫染色機"
+                },
+                "12": {
+                    "serialNo": "AK-REO 2T",
+                    "name": "AK-REO 2T常溫染色機"
+                },
+                "13": {
+                    "serialNo": "AK-REO 3T",
+                    "name": "AK-REO 3T常溫染色機"
+                },
+                "14": {
+                    "serialNo": "AK-REO 4T",
+                    "name": "AK-REO 4T常溫染色機"
+                },
+                "15": {
+                    "serialNo": "AK-REO 6T",
+                    "name": "AK-REO 6T常溫染色機"
+                },
+                "16": {
+                    "serialNo": "AK-REO 8T",
+                    "name": "AK-REO 8T常溫染色機"
+                },
+                "17": {
+                    "serialNo": "AK-QAO 250",
+                    "name": "AK-QAO 250液流染色機"
+                },
+                "18": {
+                    "serialNo": "AK-QAO 500",
+                    "name": "AK-QAO 500液流染色機"
+                },
+                "19": {
+                    "serialNo": "AK-QAO 1000",
+                    "name": "AK-QAO 1000液流染色機"
+                },
+                "20": {
+                    "serialNo": "AK-QAO 1500",
+                    "name": "AK-QAO 1500液流染色機"
+                }
+            };
+            let id = "" + i;
+            jsonFile = machineDetail;
+            ////////////////////////////////////////////////////////////////////
         }
     }
     return jsonFile;
 }
+
 function getDate(date) {
     let yyyy = date.getFullYear();
     let MM = date.getMonth() + 1;
